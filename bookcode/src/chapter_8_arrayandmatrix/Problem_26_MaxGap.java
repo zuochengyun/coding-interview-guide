8e175546b050d4c12c12d47625a5a2048559c7bd
ZuoChengyunGrowing <zuochengyun@growingio.com>
2015-11-06 16:26:29 +0800

    helloworld

---
+++ bookcode/src/chapter_8_arrayandmatrix/Problem_26_MaxGap.java
@@ -0,0 +1,58 @@
+package chapter_8_arrayandmatrix;
+
+public class Problem_26_MaxGap {
+
+	public static int maxGap(int[] nums) {
+		if (nums == null || nums.length < 2) {
+			return 0;
+		}
+		int len = nums.length;
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < len; i++) {
+			min = Math.min(min, nums[i]);
+			max = Math.max(max, nums[i]);
+		}
+		if (min == max) {
+			return 0;
+		}
+		boolean[] hasNum = new boolean[len + 1];
+		int[] maxs = new int[len + 1];
+		int[] mins = new int[len + 1];
+		int bid = 0;
+		for (int i = 0; i < len; i++) {
+			bid = bucket(nums[i], len, min, max); // 算出桶号
+			mins[bid] = hasNum[bid] ? Math.min(mins[bid], nums[i]) : nums[i];
+			maxs[bid] = hasNum[bid] ? Math.max(maxs[bid], nums[i]) : nums[i];
+			hasNum[bid] = true;
+		}
+		int res = 0;
+		int lastMax = 0;
+		int i = 0;
+		while (i <= len) {
+			if (hasNum[i++]) { // 找到第一个不空的桶
+				lastMax = maxs[i - 1];
+				break;
+			}
+		}
+		for (; i <= len; i++) {
+			if (hasNum[i]) {
+				res = Math.max(res, mins[i] - lastMax);
+				lastMax = maxs[i];
+			}
+		}
+		return res;
+	}
+
+	// 使用long类型是为了防止相乘时溢出
+	public static int bucket(long num, long len, long min, long max) {
+		return (int) ((num - min) * len / (max - min));
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 11, 10, 9, 3, 1, 12 };
+		System.out.println(maxGap(arr));
+
+	}
+
+}
